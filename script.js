931884706b6ae090d65442755fc7a5c24bc48758
Joseph Gillen <joegillen@ymail.com>
2015-11-06 01:06:28 +0000

    Changing site.css settings for web navigation
    Messing around with colours and padding sizes.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("foot01").innerHTML = "<p>&copy; Joseph Gillen</p>" + new Date().getFullYear();
 
-document.getElementById("nav01").innerHTML = "<ul id='menu'>" +
-    "<li><a href='index.html'>Home</a></li>" +
-    "<li><a href='customers.html'>About<a/></li>" +
-    "</ul>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
