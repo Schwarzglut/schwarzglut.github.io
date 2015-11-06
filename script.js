f019d9c314a76172afc24e1cb945fc53e87a2591
Joseph Gillen <joegillen@ymail.com>
2015-11-06 01:02:14 +0000

    Adding Web Navigation
    Changing between index.html and about.html

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.getElementById("foot01").innerHTML = "<p>&copy; </p>" + new Date().getFullYear();
+document.getElementById("foot01").innerHTML = "<p>&copy; Joseph Gillen</p>" + new Date().getFullYear();
+
+document.getElementById("nav01").innerHTML = "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='customers.html'>About<a/></li>" +
+    "</ul>";
