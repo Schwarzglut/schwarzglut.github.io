f831e62a88b0f1779a3e6e83d10e6834f1ed958a
Joseph Gillen <joegillen@ymail.com>
2016-09-07 19:14:31 -0400

    Adding a section About tab

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
+"<li><a href='about.html'>About 2</a></li>" +
 "</ul>";
