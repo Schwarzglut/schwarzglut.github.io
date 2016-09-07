6fd779bb88a616348258bb4ffa2cab9c72a98b2e
Joseph Gillen <joegillen@ymail.com>
2016-09-07 19:24:54 -0400

    Adding a about2.html file for use with site tabs

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
-"<li><a href='about.html'>About 2</a></li>" +
+"<li><a href='about2.html'>About 2</a></li>" +
 "</ul>";
